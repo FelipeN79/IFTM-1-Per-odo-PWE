92f31690dbf585019b6a139f972a54d3aea09254
Felipe <felipenogueira.furtado@gmail.com>
2026-09-03 21:42:23 -0300

    Adiciona projeto condominio

---
+++ MVC_SQL/Condominio/condominio/src/main/java/com/exemplo/condominio/CondominioApplication.java
@@ -0,0 +1,15 @@
+package com.exemplo.condominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CondominioApplication implements CommandLineRunner {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(CondominioApplication.class, args);
+	}
+
+}
